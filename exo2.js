390f4772f8712409fcf0841d4d13c40756d3ca04
unknown <merzouguibatna@gmail.com>
2021-04-13 02:13:14 +0200

    solt

---
+++ exo2.js
@@ -0,0 +1,105 @@
+// var heure = Number(prompt('entrez l\'heure'));
+// if(Number.isInteger(heure)) {
+//     if(heure<0) {
+//         alert('ce n\'est pas logique,veulliez entrer un numéro entre 0 et 24');
+//     }
+//     else if (heure < 12) {
+//         alert ('on est le matin');
+//     }
+//     else if (heure < 18) {
+//         alert ('on est après midi')
+//     }
+//     else if (heure <= 24) {
+//         alert ('on est en nuit');
+//     }
+//     else if(heure>24) {
+//         alert('ce n\'est pas logique,veulliez entrer un numéro entre 0 et 24');
+//     }
+// }
+// else {
+//     alert('veuillez entrer un nombre entier');
+// }
+
+// var prixDePain= 4;
+// console.log("prix unitaire du pain " + prixDePain);
+// var prixTotaldupain= prixDePain*3;
+// console.log("prix total du pain " + prixTotaldupain);
+// var prixDuGateau= 2.64;
+// console.log("prix du gateau " + prixDuGateau);
+// var argentDuClient= 20;
+// console.log("l'argent du client " + argentDuClient);
+// var laMonnaieQuiReste = argentDuClient - prixDuGateau - prixTotaldupain ;
+// console.log("le reste de l'achat " + laMonnaieQuiReste);
+// var reste = Math.round(laMonnaieQuiReste*100)/100;
+// console.log("le montant arrondi" + reste);
+// console.log("l'argent qui reste " + reste + " euros");
+
+
+
+
+//*********************** les conditions*************************************
+
+// var age = prompt("c'est quoi ton age?");
+// var prenom = prompt("comment tu t'appeles?");
+
+// if (prenom == "Jean" || prenom == "Pierrot") {
+//     alert('bienvenue patron');
+// }
+
+// else if (prenom == "Elise" || prenom == "Julie") {
+//     alert('casse toi');
+// }
+// else if (age<18)   {
+//     alert ('bienvenue!');
+// }
+// else if (age>=18) {
+//     alert("vous etes trop vieux!");
+// }
+
+// var budget = prompt('entre ton budget')//1553.89;
+// var achat = prompt('entre le montant de tes achats')//1554.76;
+// var diff = budget - achat ;
+
+// if (diff > 0 ) {
+//     alert('achat possible et il te reste ' + diff);
+// }
+// else {
+//     alert ('pas possible' );
+// }
+
+// let age = parseInt(prompt("entre ton age"));
+
+// if( age>0 && age <18) {
+//     alert('t\'es mineur');
+// }
+// else if(age >= 18) {
+//     alert('t\es majeur');
+// }
+// else {
+//     alert('arrete tes conneries');
+// }
+
+//************ les boucles***********
+
+
+
+// var codePostale= 13000;
+// while(codePostale <= 13999) {
+//     console.log (codePostale);
+//     codePostale++ ;
+// }
+
+// var mutipliant= 1;
+// var tableauDeCinq = 5*mutipliant;
+// while (mutipliant <= 10) {
+//     console.log("5 * " + mutipliant + " = " + tableauDeCinq);
+//     mutipliant++ ;
+// }
+
+
+
+
+
+
+
+
